f01e51890a7bb41317d4d51269d5143c5196c687
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2025-01-06 01:37:53 +0600

    create apis for user data save, carts related

--- index.js
+++ index.js
@@ -25,6 +25,63 @@ async function run() {
     // db collection
     const menuCollection = client.db("bistro-boss").collection("menu");
     const reviewsCollection = client.db("bistro-boss").collection("reviews");
+    const cartsCollection = client.db("bistro-boss").collection("carts");
+    const usersCollection = client.db("bistro-boss").collection("users");
+
+    // users related apis
+    app.patch("/users/:email", async (req, res) => {
+      try {
+        const userData = req.body;
+        const email = req.params.email;
+        const isExist = await usersCollection.findOne({ email });
+        if (isExist) {
+          res
+            .status(200)
+            .json({ message: "User already exist", data: isExist._id });
+          return;
+        }
+        const result = await usersCollection.insertOne(userData);
+        res.status(201).json({
+          success: true,
+          message: "Successfully user created",
+          data: result,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    // cats related apis
+    app.post("/carts", async (req, res) => {
+      try {
+        const cartData = req.body;
+        const result = await cartsCollection.insertOne(cartData);
+        res.status(201).json({
+          success: true,
+          message: "Item successfully added to cart",
+          data: result,
+        });
+      } catch (error) {
+        res
+          .status(500)
+          .send({ message: "Sever error. Please try again later." });
+      }
+    });
+
+    app.get("/carts/:email", async (req, res) => {
+      try {
+        const email = req.params.email;
+        const query = { email };
+        const result = await cartsCollection.find(query).toArray();
+        res.status(200).json({
+          success: true,
+          message: "Carts list fetching success.",
+          data: result,
+        });
+      } catch (error) {
+        res.status(500).send({ message: "Server error. Please try again." });
+      }
+    });
 
     // menu related apis
     app.get("/menu", async (req, res) => {
