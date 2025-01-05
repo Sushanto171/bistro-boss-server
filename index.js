ae2daad5ebe5a68fafa254c09c7a179a351d53f0
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2025-01-06 02:58:45 +0600

    implement jwt and token verify

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PROT || 5000;
@@ -8,6 +10,7 @@ const app = express();
 
 // middleware
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 
 const uri = process.env.DB_URL;
@@ -27,6 +30,44 @@ async function run() {
     const reviewsCollection = client.db("bistro-boss").collection("reviews");
     const cartsCollection = client.db("bistro-boss").collection("carts");
     const usersCollection = client.db("bistro-boss").collection("users");
+
+    // generate token
+    app.post("/jwt", async (req, res) => {
+      const key = process.env.JWT_ACCESS_SECRET;
+      const user = req.body;
+      const token = jwt.sign(user, key, { expiresIn: "1hr" });
+      res.status(200).json({
+        message: "token created success",
+        data: token,
+      });
+    });
+
+    // clear token
+    app.get("/log-out", async (req, res) => {});
+
+    // verifyToken
+    const verifyToken = async (req, res, next) => {
+      try {
+        const authHeader = req.headers["authorization"];
+        const [schema, token] = authHeader.split(" ");
+        if (schema !== "Bearer" || !token) {
+          return res
+            .status(403)
+            .json({ message: "Forbidden unauthorized access" });
+        }
+        jwt.verify(token, process.env.JWT_ACCESS_SECRET, (error, decoded) => {
+          if (error) {
+            return res
+              .status(403)
+              .json({ message: "Forbidden unauthorized access" });
+          }
+          req.user = decoded.email;
+          next();
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
     // users related apis
     app.patch("/users/:email", async (req, res) => {
@@ -68,8 +109,10 @@ async function run() {
       }
     });
 
-    app.get("/carts/:email", async (req, res) => {
+    app.get("/carts/:email", verifyToken, async (req, res) => {
       try {
+        const user = req.user;
+        console.log(user);
         const email = req.params.email;
         const query = { email };
         const result = await cartsCollection.find(query).toArray();
